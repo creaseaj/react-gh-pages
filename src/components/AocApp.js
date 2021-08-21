8f4dce4b2feb521383da85d3db1e4a74afb6c0d9
creaseaj <creaseaj@gmail.com>
2021-08-21 16:31:52 +0100

    added mobile support

--- src/components/AocApp.js
+++ src/components/AocApp.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 const ShowList = (props) => {
     let output = [];
     for (let i = 1; i <= 25; i++) {
-        output.push(<div className="text-lg hover:bg-gray-700 hover:text-white" id={i}
+        output.push(<div className="text-lg hover:bg-gray-700 hover:text-white" id={i} key={i}
             onClick={(e) => {
                 props.setSelectedDay(e.target.id)
                 props.setShowSelectedDay(false)
@@ -19,18 +19,18 @@ const AocApp = () => {
     const [showSelectedDay, setShowSelectedDay] = useState(false)
     const [selectedDay, setSelectedDay] = useState(null)
     return (
-        <div className=" w-screen h-screen flex items-center -m-16">
+        <div className=" w-screen h-screen flex items-center sm:-mt-16">
             {showSelectedDay ? (
-                <div className=" z-10 absolute right-0 bottom-0 top-0 left-0" onClick={() => setShowSelectedDay(!showSelectedDay)} />
+                <div className=" z-20 absolute right-0 bottom-0 top-0 left-0" onClick={() => setShowSelectedDay(!showSelectedDay)} />
             ) : null}
-            <div className="relative flex container mx-auto h-app bg-gray-200 rounded-xl p-10 justify-end">
+            <div className="relative flex sm:container sm:mx-auto w-screen h-screen sm:h-app bg-gray-200 sm:rounded-xl p-10 justify-end">
                 <div className="flex h-10 w-56 border border-gray-800 rounded-md justify-center items-center hover:bg-gray-800 hover:text-gray-200 self-end"
                     onClick={() => setShowSelectedDay(!showSelectedDay)}
                 >
                     {selectedDay == null ? 'Click to pick a day' : 'Day ' + selectedDay}
                 </div>
                 {showSelectedDay ? (
-                    <div className=" z-20 absolute right-0 bottom-0  border-2 rounded-xl border-gray-700 bg-gray-100 w-72 divide-y ">
+                    <div className=" z-30 absolute right-0 bottom-0  border-2 rounded-xl border-gray-700 bg-gray-100 w-72 divide-y ">
                         <ShowList setShowSelectedDay={setShowSelectedDay} setSelectedDay={setSelectedDay} />
                     </div>
                 ) : null}
