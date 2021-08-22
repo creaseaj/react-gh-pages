3401e72b90967ab8c3bc0e969d09689112e5cb94
creaseaj <creaseaj@gmail.com>
2021-08-22 12:46:16 +0100

    added nullable options on the day picker

--- src/components/AocApp.js
+++ src/components/AocApp.js
@@ -2,20 +2,50 @@ import React, { useState } from 'react';
 
 const ShowList = (props) => {
     let output = [];
-    for (let i = 1; i <= 25; i++) {
+    Days.forEach((day,i) => {
         output.push(
             <div
-                className={`text-lg bg-gray-100 hover:bg-gray-700 hover:text-white cursor-pointer border-b border-l-[2px] border-r-[2px] border-gray-700 ${i == 1 ? 'rounded-t-xl border-t-[2px]' : i == 25 ? 'rounded-b-xl border-b-[2px]' : null}`} id={i} key={i}
+                className={`${day == null ? 'bg-gray-200' : ' bg-gray-100 hover:bg-gray-700 hover:text-white'} text-lg  mx-[-2px]  cursor-pointer border-b border-gray-700 ${i == 0 ? 'rounded-t-xl mt-[-2px]' : i == 24 ? 'rounded-b-xl mb-[-2px]' : null}`} id={i + 1} key={i + 1}
                 onClick={(e) => {
-                    props.setSelectedDay(e.target.id)
-                    props.setShowSelectedDay(false)
+                    if(day !== null) {
+                        props.setSelectedDay(e.target.id)
+                        props.setShowSelectedDay(false)
+                    }
                 }}>
-                Day {i}
+                Day {i + 1}
             </div>
         )
-    }
+    })
     return output
 }
+
+const Days = [
+    null,
+    '2',
+    null,
+    '2',
+    '3',
+    null,
+    '1',
+    null,
+    '3',
+    '3',
+    null,
+    '3',
+    '2',
+    '3',
+    '3',
+    '3',
+    '3',
+    '3',
+    '3',
+    '3',
+    '3',
+    '3',
+    '3',
+    '3',
+    null
+]
 
 const AocApp = () => {
     const [showSelectedDay, setShowSelectedDay] = useState(false)
@@ -25,17 +55,17 @@ const AocApp = () => {
             {showSelectedDay ? (
                 <div className=" z-20 absolute right-0 bottom-0 top-0 left-0" onClick={() => setShowSelectedDay(!showSelectedDay)} />
             ) : null}
-            <div className="relative flex sm:container sm:mx-auto w-screen h-full sm:h-[80vh] bg-gray-200 sm:rounded-xl p-10 gap-[50px]">
-                <div className="w-full  justify-between justify-self-end self-end flex">
-                    <div className="flex h-10 w-full border border-gray-700 rounded-md justify-center items-center hover:bg-gray-700 hover:text-gray-200 "> Test</div>
-                    <div className="flex h-10 w-full border border-gray-700 rounded-md justify-center items-center hover:bg-gray-700 hover:text-gray-200 "> Test</div>
+            <div className="relative flex sm:container sm:mx-auto w-screen h-full sm:h-[80vh] bg-gray-200 sm:rounded-xl p-10 ">
+                <div className="w-full  justify-between justify-self-end self-end flex gap-[50px]">
+                    <div className="flex h-10 w-full border border-gray-700 rounded-md justify-center items-center hover:bg-gray-700 hover:text-gray-200 ">Part 1</div>
+                    <div className="flex h-10 w-full border border-gray-700 rounded-md justify-center items-center hover:bg-gray-700 hover:text-gray-200 ">Part 2</div>
                     <div className="flex h-10 w-full border border-gray-700 rounded-md justify-center items-center hover:bg-gray-700 hover:text-gray-200 "
                         onClick={() => setShowSelectedDay(!showSelectedDay)}
                     >
                         {selectedDay == null ? 'Click to pick a day' : 'Day ' + selectedDay}
                     </div>
                     {showSelectedDay ? (
-                        <div className="z-30 absolute right-4 bottom-4    rounded-x w-72">
+                        <div className="z-30 absolute right-4 bottom-4 max-h-[calc(80vh-2rem)] overflow-x-hidden overflow-y-auto border-[2px] scrollbar-hide border-gray-700 rounded-xl w-72">
                             <ShowList setShowSelectedDay={setShowSelectedDay} setSelectedDay={setSelectedDay} />
                         </div>
                     ) : null}
