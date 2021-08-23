e3775fc6c690071bdddd1759f4a55b94ccff6a1e
creaseaj <creaseaj@gmail.com>
2021-08-23 21:04:20 +0100

    animated change day

--- src/components/AocApp.js
+++ src/components/AocApp.js
@@ -1,15 +1,15 @@
 import React, { useEffect, useState, useRef } from 'react';
-import { Transition } from '@headlessui/react'
 import dayFunctions, { day18p1, day1p1, day1p2 } from './dayFunctions';
 import AocTestCode from './AocTestCode';
 import Button from './Button';
+import { motion } from "framer-motion"
 
 const ShowList = (props) => {
     let output = [];
     Days.forEach((day, i) => {
         output.push(
             <div
-                className={`${day === null ? 'bg-gray-200' : ' bg-gray-100 hover:bg-gray-700 hover:text-white'} text-lg  mx-[-2px]  cursor-pointer border-b border-gray-700 ${i === 0 ? 'rounded-t-xl mt-[-2px]' : i === 24 ? 'rounded-b-xl mb-[-2px]' : null}`} id={i + 1} key={i + 1}
+                className={`${day === null ? 'bg-gray-200' : ' bg-gray-100 hover:bg-gray-700 hover:text-white'} ${i !== 24 ? 'border-b' : null} text-lg  mx-[-2px]  cursor-pointer  border-gray-700 ${i === 0 ? 'rounded-t-xl mt-[-2px]' : i === 24 ? 'rounded-b-xl mb-[-2px]' : null}`} id={i + 1} key={i + 1}
                 onClick={(e) => {
                     if (day !== null) {
                         props.setSelectedDay(e.target.id)
@@ -78,14 +78,12 @@ const AocApp = () => {
         leave: "leave",
         leaveFrom: "leaveFrom",
         leaveTo: "leaveTo"
-      };
-      useEffect(() => {
-        console.log('width', changeDayButton.current ? changeDayButton.current.offsetWidth : 0);
-      }, [changeDayButton]);
-      return (
+    };
+    console.log(showSelectedDay)
+    return (
         <div className=" w-screen h-[calc(100vh-4rem)] sm:h-screen flex items-center sm:-mt-16">
             {showSelectedDay ? (
-                <div className=" z-20 absolute right-0 bottom-0 top-0 left-0" onClick={() => setShowSelectedDay(!showSelectedDay)} />
+                <div className=" z-10 absolute right-0 bottom-0 top-0 left-0" onClick={() => setShowSelectedDay(!showSelectedDay)} />
             ) : null}
             <div className="relative flex sm:container sm:mx-auto w-screen h-full sm:h-[80vh] bg-gray-200 sm:rounded-xl p-[10px] flex-col justify-between gap-[10px]">
                 <div className="w-full flex justify-between text-lg">
@@ -110,15 +108,29 @@ const AocApp = () => {
                 <div className="w-full  justify-between justify-self-end self-end flex flex-col sm:flex-row gap-[10px] sm:gap-[50px]">
                     <Button function={() => setSolution(Days[selectedDay - 1].solution1(input))} value="Part 1" />
                     <Button function={() => setSolution(Days[selectedDay - 1].solution2(input))} value="Part 2" disabled={Days[selectedDay - 1].solution2 == null} />
-                    <Button id="dayPick" function={() => setShowSelectedDay(!showSelectedDay)} value='Change Day'  ref={changeDayButton}/>
-                    <Transition
-                        className="z-30 absolute right-4 bottom-4 max-h-[calc(80vh-2rem)] overflow-x-hidden overflow-y-auto border-[2px] scrollbar-hide border-gray-700 rounded-xl"
-                        style={{width: changeDayButton.current?.offsetWidth ?? 400 }}
+                    <div ref={changeDayButton} className="z-30 w-full">
+                        <Button id="dayPick" function={() => setShowSelectedDay(!showSelectedDay)} value='Change Day' />
+
+                    </div>
+                    <motion.div
+                        className={`bg-gray-200 z-20 absolute right-[10px] bottom-[10px] max-h-[calc(80vh-2rem)] overflow-x-hidden overflow-y-auto scrollbar-hide border-[2px] border-gray-700 rounded-xl  ${showSelectedDay ? ' pb-[40px]' : 'invisible'}`}
+                        style={{ width: changeDayButton.current?.offsetWidth ?? 400 }}
+                        animate={{
+                            height: !showSelectedDay ? 40 : "auto"
+
+                        }}
+                    >
+                        <ShowList setShowSelectedDay={setShowSelectedDay} setSelectedDay={setSelectedDay} />
+                    </motion.div>
+
+                    {/* <Transition
+                        className="z-10 absolute right-[10px] bottom-[10px] max-h-[calc(80vh-2rem)] overflow-x-hidden overflow-y-auto border-[2px] scrollbar-hide border-gray-700 rounded-xl pb-[40px]"
+                        style={{ width: changeDayButton.current?.offsetWidth ?? 400 }}
                         show={showSelectedDay}
                         {...classNames}
                     >
                         <ShowList setShowSelectedDay={setShowSelectedDay} setSelectedDay={setSelectedDay} />
-                    </Transition>
+                    </Transition> */}
                 </div>
 
             </div>
--- src/components/Button.js
+++ src/components/Button.js
@@ -2,9 +2,9 @@
 const Button = (props) => {
     console.log(props.ref)
     return (
-        <div className={`${props.disabled === true ? 'bg-gray-400 text-gray-50' : 'border border-gray-700 hover:bg-gray-700 hover:text-gray-200' } flex h-10 w-full rounded-md  justify-center items-center `}
+        <div className={`${props.disabled === true ? 'bg-gray-400 text-gray-50' : 'bg-gray-200 border border-gray-700 hover:bg-gray-700 hover:text-gray-200'} flex h-10 w-full rounded-md  justify-center items-center `}
             onClick={props.disabled ? null : props.function}
-            ref={props.ref == null ?  null : props.ref}
+            ref={props.ref == null ? null : props.ref}
         >
             {props.value}
         </div>
