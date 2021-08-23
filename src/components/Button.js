dc11eb2be06250250182eecdb769ef198769c766
creaseaj <adam@coreblue.co.uk>
2021-08-23 12:31:37 +0100

    improved animations, ref can now be passed to a button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,10 @@
 
 const Button = (props) => {
+    console.log(props.ref)
     return (
         <div className={`${props.disabled === true ? 'bg-gray-400 text-gray-50' : 'border border-gray-700 hover:bg-gray-700 hover:text-gray-200' } flex h-10 w-full rounded-md  justify-center items-center `}
             onClick={props.disabled ? null : props.function}
+            ref={props.ref == null ?  null : props.ref}
         >
             {props.value}
         </div>
