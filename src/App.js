8286063936aa4e5d84117c879761223ec364ddd5
creaseaj <creaseaj@gmail.com>
2021-11-13 14:28:29 +0000

    added background shapes and year support

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
+import { useState } from 'react';
 import './App.css';
 import AocApp from './components/AocApp';
 import Header from './components/Header';
 
 function App() {
+  const [year, setYear] = useState(2021);
+  console.log(year);
   return (
-    <div className="App">
-      <Header />
-      <AocApp />
-    </div>
+    <div className={"App h-screen transition-all " + (year === 2021 ? "bg-[#ffefdb]" : "bg-white")}>
+      <Header setYear={(e) => setYear(e)} year={year} />
+      <AocApp year={year} />
+      <div className={"fixed -rotate-45 w-72 h-8 top-[30%] right-[-10rem] rounded-full transition-all  border-2  " + (year === 2021 ? "bg-red-300 border-gray-900" : "bg-transparent border-transparent")} />
+      <div className={"fixed -rotate-45 w-72 h-8 top-[40%] right-[-9rem] rounded-full transition-all  border-2  " + (year === 2021 ? "bg-yellow-300 border-gray-900" : "bg-transparent border-transparent")} />
+      <div className={"fixed -rotate-45 w-72 h-8 top-1/2 right-[-8rem] rounded-full transition-all  border-2  " + (year === 2021 ? "bg-blue-300 border-gray-900" : "bg-transparent border-transparent")} />
+      <div className={"fixed -rotate-45 w-72 h-8 top-[60%] right-[-7rem] rounded-full transition-all  border-2  " + (year === 2021 ? "bg-purple-300 border-gray-900" : "bg-transparent border-transparent")} />
+      <div className={"fixed left-[-20rem] top-1/3 h-[50rem] w-[50rem] rounded-full  flex items-center justify-center from-green-300 to-blue-300 transition-all  border-2  " + (year === 2021 ? "bg-gradient-to-t border-gray-900" : "bg-transparent border-transparent")}>
+        <div className={"w-[45rem] h-[45rem] rounded-full transition-all  border-2  " + (year === 2021 ? "bg-[#ffefdb] border-gray-900" : "bg-transparent border-transparent")} />
+      </div>
+      <div className={"fixed h-[40rem] w-72  from-pink-300 to-purple-300 top-[-12.5rem] left-[50%] rotate-[75deg] rounded-md  border-2  " + (year === 2021 ? "bg-gradient-to-t border-gray-900" : "bg-transparent border-transparent")}></div>
+
+
+    </ div>
   );
 }
 
